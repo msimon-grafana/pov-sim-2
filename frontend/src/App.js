e8bd38ee5419d71a4395d25e5d6c8a58adc5f877
msimon-grafana <mike.simon@grafana.com>
2026-03-02 20:55:14 +0000

    added pyroscope

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,25 @@ import Home from './pages/Home';
 import Airlines from './pages/Airlines';
 import Flights from './pages/Flights';
 import Navigation from './components/Navigation';
+import { getWebInstrumentations, initializeFaro } from '@grafana/faro-web-sdk';
+import { TracingInstrumentation } from '@grafana/faro-web-tracing';
 
+initializeFaro({
+  url: 'https://faro-collector-prod-us-east-1.grafana.net/collect/3648756f5ae493ee16070dceb1856a44',
+  app: {
+    name: 'POV-SIM',
+    version: '1.0.0',
+    environment: 'production'
+  },
+  
+  instrumentations: [
+    // Mandatory, omits default instrumentations otherwise.
+    ...getWebInstrumentations(),
+
+    // Tracing package to get end-to-end visibility for HTTP requests.
+    new TracingInstrumentation(),
+  ],
+});
 function App() {
   return (
     <div className="App">
